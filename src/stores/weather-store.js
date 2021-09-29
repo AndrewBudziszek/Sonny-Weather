a026f5f249f263c9462baf80d7bcf7dc6ac796d4
Andrew Budziszek <andrew@budziszek.com>
2021-09-29 10:58:27 -0500

    using alternate openweather api key

--- src/stores/weather-store.js
+++ src/stores/weather-store.js
@@ -4,8 +4,8 @@ export const weather = writable([]);
 
 export const fetchWeather = async (lat, lon) => {
     if(lat && lon) {
-        const apikey = import.meta.env.VITE_OPENWEAHTER_API_KEY
-        //const apikey = import.meta.env.VITE_OPENWEAHTER_API_KEY_ALT; //ALT
+        //const apikey = import.meta.env.VITE_OPENWEAHTER_API_KEY
+        const apikey = import.meta.env.VITE_OPENWEAHTER_API_KEY_ALT; //ALT
         const url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}`;
         const res = await fetch(url);
         const data = await res.json();
